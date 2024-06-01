10ee6fa64cfadbc7b17ff505ca782ad26081ef98
jazztinecruz <me@jazztinecruz.com>
2024-06-01 22:29:05 +0800

    feat: create showcase card and carousel to it

---
+++ app/_components/sections/projects/card.tsx
@@ -0,0 +1,93 @@
+"use client";
+
+import { Design, Development } from "@/libraries/types";
+import {
+  ArrowUpRightIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+} from "@heroicons/react/24/solid";
+import Link from "next/link";
+import { useState } from "react";
+
+type Props = {
+  data: Design | Development;
+  type: "design" | "development";
+};
+
+const ShowcaseCard = ({ data, type = "development" }: Props) => {
+  const [currentDisplayIndex, setCurrentDisplayIndex] = useState<number>(0);
+
+  const photos: number[] = Array.from({ length: 5 }, (_, index) => index + 1);
+
+  const handleNext = () => {
+    if (currentDisplayIndex < photos.length - 1) {
+      setCurrentDisplayIndex((prev) => prev + 1);
+    } else setCurrentDisplayIndex(0);
+  };
+
+  const handlePrev = () => {
+    if (currentDisplayIndex < 1) setCurrentDisplayIndex(photos.length - 1);
+    else setCurrentDisplayIndex((prev) => prev - 1);
+  };
+
+  const previews = () => {
+    switch (type) {
+      case "development": {
+        const code = data as Development;
+        return (
+          <div className="flex flex-wrap items-end gap-2">
+            {code.url && <Badge label="Preview" url={code.url} />}
+            <Badge label="Github" url={code.github} />
+            {code.ongoing && <Badge label="In Development" />}
+          </div>
+        );
+      }
+      case "design": {
+        const design = data as Design;
+        return <Badge label="Preview" url={design.pdf} />;
+      }
+    }
+  };
+
+  return (
+    <div className="flex flex-col rounded-md">
+      {/* carousel */}
+      <div className="relative aspect-video w-full h-full bg-gray-500 rounded-t-md">
+        <button
+          onClick={handlePrev}
+          className="absolute left-2 top-2/4 -translate-y-2/4 bg-opacity rounded p-1">
+          <ChevronLeftIcon className="w-5 h-5" />
+        </button>
+        <div>{photos[currentDisplayIndex]}</div>
+        <button
+          onClick={handleNext}
+          className="absolute right-2 top-2/4 -translate-y-2/4 bg-opacity rounded p-1">
+          <ChevronRightIcon className="w-5 h-5" />
+        </button>
+      </div>
+      {/* details */}
+      <div className="bg-opacity p-4 space-y-2 rounded-b-md grid">
+        <h3 className="font-semibold">{data.title}</h3>
+        <p className="text-sm tertiary mb-4">{data.description}</p>
+        {previews()}
+      </div>
+    </div>
+  );
+};
+
+export default ShowcaseCard;
+
+const Badge = ({ label, url }: { label: string; url?: string }) => {
+  const backgroundColor =
+    label === "In Development" ? "bg-yellow-600/50" : "bg-green-700/50";
+
+  return (
+    <Link href={url || ""} target="_blank">
+      <div
+        className={`text-xs rounded-full flex items-center gap-1 py-1 px-3 w-fit font-semibold ${backgroundColor}`}>
+        <span>{label}</span>
+        {url && <ArrowUpRightIcon title="Download PDF" className="w-3 h-3" />}
+      </div>
+    </Link>
+  );
+};
--- app/_components/sections/projects/index.tsx
+++ app/_components/sections/projects/index.tsx
@@ -1,9 +1,6 @@
 import { projects } from "@/libraries/constants/data";
 import { SectionTitle } from "../../titles";
-import { ArrowUpRightIcon } from "@heroicons/react/24/solid";
-import Link from "next/link";
-import Github from "../../github";
-import Image from "next/image";
+import ShowcaseCard from "./card";
 
 const Projects = () => {
   return (
@@ -16,67 +13,15 @@ const Projects = () => {
         <h2 className="font-bold text-lg">Developments</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-8">
           {projects.development.map((code) => (
-            <div
-              key={code.title}
-              className="relative w-full h-48 lg:h-56 rounded-md  overflow-hidden">
-              <div className="absolute h-[50%] inset-0 rounded-md bg-gradient-to-r from-transparent via-darkblue to-darkpink animate-spin-slow" />
-              <div className="absolute inset-[0.07rem] primary p-4 flex flex-col justify-center gap-4 py-4 lg:py-6 rounded-md">
-                <div className="flex items-center gap-3">
-                  <h3 className="font-semibold">{code.title}</h3>
-                  {code.url && (
-                    <Link
-                      href={code.url}
-                      target="_blank"
-                      rel="noopener noreferrer">
-                      <ArrowUpRightIcon title="Live Url" className="w-5 h-5" />
-                    </Link>
-                  )}
-                </div>
-                <p className="text-sm tertiary">{code.description}</p>
-                <Github url={code.github} hasArrow />
-                {code.ongoing && (
-                  <div className="bg-green-700/30 rounded px-3 py-1 w-fit text-xs text-green-700 font-semibold -mt-2">
-                    In Development
-                  </div>
-                )}
-              </div>
-            </div>
+            <ShowcaseCard key={code.title} data={code} type="development" />
           ))}
         </div>
       </div>
-
       <div className="space-y-4">
         <h2 className="font-bold text-lg">Designs</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 rounded-t-md">
           {projects.design.map((design) => (
-            <div key={design.title} className="flex flex-col rounded-md">
-              <Link href={design.pdf} target="_blank" rel="noopener noreferrer">
-                <div className="aspect-video relative w-full h-full">
-                  <Image
-                    src={design.thumbnail}
-                    alt={design.title}
-                    fill
-                    priority
-                    className="rounded-t-md object-cover"
-                  />
-                </div>
-              </Link>
-              <div className="bg-opacity p-4 space-y-2 rounded-b-md">
-                <div className="flex items-center gap-3">
-                  <h3 className="font-semibold">{design.title}</h3>
-                  <Link
-                    href={design.pdf}
-                    target="_blank"
-                    rel="noopener noreferrer">
-                    <ArrowUpRightIcon
-                      title="Download PDF"
-                      className="w-5 h-5"
-                    />
-                  </Link>
-                </div>
-                <p className="text-sm tertiary">{design.description}</p>
-              </div>
-            </div>
+            <ShowcaseCard key={design.title} data={design} type="design" />
           ))}
         </div>
       </div>
